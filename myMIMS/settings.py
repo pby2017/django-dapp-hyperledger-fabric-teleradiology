670e58eec506745411342b47fbcfe292b2b01749
ByungYongPark <pby2017@github.com>
2018-12-27 09:02:26 +0900

    change db settings sqlite3 to mysql

--- myMIMS/settings.py
+++ myMIMS/settings.py
@@ -74,20 +74,23 @@ WSGI_APPLICATION = 'myMIMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mims'
+#         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mims',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': ""
+    }
+}
 
 
 # Password validation
